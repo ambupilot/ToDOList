bc11e1fa0abf535109b1702e82299c0b06039ac0
Martijn Kerssing <martijn@kerssing.nl>
2020-04-03 16:38:37 +0200

    add to DB

--- script.js
+++ script.js
@@ -4,18 +4,12 @@ const buttonAddTask = document.getElementById('buttonAddTask');
 const ulTasks = document.getElementById('tasks');
 const deleteButtons = document.getElementsByClassName('deleteBtn');
 
-// postItem(description, done) => post item
-// deleteItem(id) => delete item met het meegegeven id token
-
 const getToDos = async function() {
     const data = await getData('tasks.json');
-    // console.log("toDos data: ", data);
+    emptyList(); //start clean
     // iterate over tasks
     data.forEach(element => {
-        //
         // console.log(element.id);
-        // console.log(element.description);
-        // console.log(element.done);
         const tasksList = document.getElementById('tasks');
         // create li element
         const li = document.createElement('li');
@@ -27,13 +21,13 @@ const getToDos = async function() {
         check.type = 'checkbox';
         check.className = 'check';
         check.id = `${element.id}`;
-
+        // create deletebutton
         const deleteBtn = document.createElement('input');
         deleteBtn.type = 'image';
         deleteBtn.value = `${element.id}`
         deleteBtn.className = 'deleteBtn';
         deleteBtn.src = 'trash-delete-icon.jpg';
-
+        // add to list
         li.appendChild(check);
         li.appendChild(liText)
         li.appendChild(deleteBtn)
@@ -42,34 +36,38 @@ const getToDos = async function() {
     })
 }
 
-// const deleteItem = function(id) {
-const deleteToDo = function() {}
+// functies die een todo toevoegen en verwijderen:
+// postItem(description, done) => post item
+// deleteItem(id) => delete item (!! met het meegegeven id token)
 
+// na klikken op deknop 'add' moet inputfield worden gepakt en naar de functie postItem() worden verzonden
+// eventlistners:
+inputNewTask.addEventListener('change', function(e) {
+    console.log(e.target.value);
+    let inputText = e.target.value;
+    inputToAdd(inputText)
+} )
 
-// Function clear list -> not db. (test purposes = reload after add/delete)
-const emptyList = function () {
-    ulTasks.querySelectorAll('li').forEach(n => n.remove())
+inputToAdd = function(text) {
+    buttonAddTask.addEventListener('click', function(e) {
+        let knop = e.target.value;
+        console.log("je klikt op: ", knop);
+        console.log(text);
+        postItem(text, false)
+        setTimeout(function(){
+            location.reload();
+         }, 1000)
+        })
 }
 
-// eventlistners:
-buttonAddTask.addEventListener('click', function() {
-    console.log('test');    
-   emptyList();
-})
-
-// deletebuttons: eerst array van maken
-
-const addElToDeleteBtn = async function() {
-Array.from(deleteButtons).forEach(function (btn) {
-    btn.addEventListener('click', function () {
-        emptyList(); // updaten naar het specifieke item uit de lijst
-    })
-})
+// Function clear list (not db, alleen op DOM)
+// (test purposes EN voor  reload after add/delete)
+const emptyList = function () {
+    ulTasks.querySelectorAll('li').forEach(n => n.remove())
 }
 
 // wachten tot DOM en alles geladen is VOOR uitvoeren
 document.addEventListener("DOMContentLoaded", () => {
     // welke functies moeten worden geladen na laden:
     getToDos();
-    addElToDeleteBtn()
 });
