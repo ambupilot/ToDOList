292e8caa950874fe8da526839b97b185d7fc6b5f
Martijn Kerssing <martijn@kerssing.nl>
2020-04-03 10:16:35 +0200

    basis voor cummunicatie met API

---
+++ api-client.js
@@ -0,0 +1,25 @@
+const API_URL = 'https://wincacademydatabase.firebaseio.com/martijn/';
+
+// Declareer een asynchronous functie getData() met het async keyword
+const getData = async function (endPoint) {
+    const apiUrl = `${API_URL}${endPoint}`;
+    //console.log(apiUrl);
+    
+    try {
+        const res = await fetch(apiUrl, {method: "GET"});
+        // console.log("Response van API", res);
+        const result = await res.json();
+        //console.log("Before (the raw result):", result);
+
+        let tasks = Object.keys(result).map(key => ({
+            id: key,
+            description: result[key].description,
+            done: result[key].done
+        }));
+        //console.log("After the tasks array", tasks);
+
+        return result;
+    }   catch (error) {
+        console.log(error);        
+    }
+}
---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+
+// wachten tot DOM en alles geladen is VOOR uitvoeren
+document.addEventListener("DOMContentLoaded", () => {
+    // welke functies moeten worden geladen na laden:
+    getData('tasks.json')    
+});
