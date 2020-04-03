5b2ec1bbcfc0148d2cd8a735736a07dbc4e9dbdd
Martijn Kerssing <martijn@kerssing.nl>
2020-04-03 14:27:28 +0200

    POST en DELETE functions OK

--- api-client.js
+++ api-client.js
@@ -21,3 +21,44 @@ const getData = async function (endPoint) {
         console.log(error);        
     }
 }
+// Item POSTEN naar API
+const postItem = function(description, done) {
+    let myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    let raw = JSON.stringify({"description":`${description}`,"done":`${done}`});
+
+    let requestOptions = {
+      method: 'POST',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+
+    fetch("https://wincacademydatabase.firebaseio.com/martijn/tasks.json", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+}
+// postItem('afwas doen', 'false') ==> werkt
+
+// Item DELETEN van API (thanks to postman docs :-) )
+const deleteItem = function(id) {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    
+    var raw = JSON.stringify({id:{}});
+    
+    var requestOptions = {
+      method: 'DELETE',
+      headers: myHeaders,
+      body: raw,
+      redirect: 'follow'
+    };
+    
+    fetch(`https://wincacademydatabase.firebaseio.com/martijn/tasks/${id}.json`, requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+}
+// deleteItem('-M3zveP8HbGKfWZcdWPp') ==> werkt
--- script.js
+++ script.js
@@ -1,21 +1,63 @@
+// references:
+const inputNewTask = document.getElementById('inputNewTask');
+const buttonAddTask = document.getElementById('buttonAddTask');
+const ulTasks = document.getElementById('tasks');
+const deleteButtons = document.getElementsByClassName('deleteBtn');
+
 const getToDos = async function() {
     const data = await getData('tasks.json');
     // console.log("toDos data: ", data);
     // iterate over tasks
     data.forEach(element => {
-       // console.log(element.id);
-       // console.log(element.description);
-       // console.log(element.done);
-       const tasksList = document.getElementById('tasks');
-       // create li element
-       const li = document.createElement('li');
-       const liText = document.createTextNode(
-           `${element.description}`
-       );
-       li.appendChild(liText);
-       tasksList.append(li)
+        //
+        // console.log(element.id);
+        // console.log(element.description);
+        // console.log(element.done);
+        const tasksList = document.getElementById('tasks');
+        // create li element
+        const li = document.createElement('li');
+        const liText = document.createTextNode(
+            `${element.description}`
+        );
+        // create checkbox
+        const check = document.createElement('input');
+        check.type = 'checkbox';
+        check.className = 'check';
+        check.id = `${element.id}`;
+
+        const deleteBtn = document.createElement('input');
+        deleteBtn.type = 'image';
+        deleteBtn.value = `${element.id}`
+        deleteBtn.className = 'deleteBtn';
+        deleteBtn.src = 'trash-delete-icon.jpg';
+
+        li.appendChild(check);
+        li.appendChild(liText)
+        li.appendChild(deleteBtn)
+        tasksList.append(li)
 
     })
+}
+
+
+
+// Function clear list (test purposes = reload after add/delete)
+const emptyList = function () {
+    ulTasks.querySelectorAll('li').forEach(n => n.remove())
+}
+
+// eventlistners:
+buttonAddTask.addEventListener('click', function() {
+   emptyList();
+})
+
+// deletebuttons: eerst array van maken
+const addElToDeleteBtn = async function() {
+Array.from(deleteButtons).forEach(function (btn) {
+    btn.addEventListener('click', function () {
+        emptyList(); // updaten naar het specifieke item uit de lijst
+    })
+})
 }
 
 
@@ -24,4 +66,11 @@ const getToDos = async function() {
 document.addEventListener("DOMContentLoaded", () => {
     // welke functies moeten worden geladen na laden:
     getToDos();
+    addElToDeleteBtn();
 });
+
+/*
+deleteButton.addEventListener('click', function()  {
+    emptyList();
+})
+*/
